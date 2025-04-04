1e225b10f8a959ac598a850038ffc817902fd119
Lucas Jacquemin <lucas.jacquemin2002@gmail.com>
2025-04-04 20:06:26 +0200

    level 3 done

--- Module01/Assets/Scripts/StickyFeet.cs
+++ Module01/Assets/Scripts/StickyFeet.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class StickyFeet : MonoBehaviour
@@ -31,7 +32,11 @@ public class StickyFeet : MonoBehaviour
     void Update()
     {
         if(ground){
-            player.position += ground.position - old_pos;
+            Vector3 diff = ground.position - old_pos;
+            if (Mathf.Abs(diff.y) > 1)
+                player.position += diff;
+            else
+                player.position += new Vector3(diff.x, 0, 0);
             old_pos = ground.position;
         }
     }
